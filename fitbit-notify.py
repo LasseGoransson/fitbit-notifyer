d3109617acec68a036ca186d06012ea36ee16e05
Lasse G <lasse973@gmail.com>
2021-03-01 21:18:32 +0100

    updates

--- fitbit-notify.py
+++ fitbit-notify.py
@@ -16,7 +16,21 @@ def getMissingSteps(authd_client):
 def formatMsg(missingSteps):
     return f'Du mangler kun {missingSteps:d} skridt!'
 
+def refresh_cb(token_dict):
+    """Function for refreshing access_token, refresh_token, and expires_at."""
+    global cfg
+    cfg['fitbit_tokens']['access_token'] = token_dict['access_token']
+    cfg['fitbit_tokens']['expires_at'] = str(token_dict['expires_at'])
+    cfg['fitbit_tokens']['refresh_token'] = token_dict['refresh_token']
+
+    with open(r'config.yml', 'w') as file:
+        documents = yaml.dump(cfg, file)
+
+    return token_dict
+
+
 def notify(cfg):
+
 
     fitbit_tokens = cfg['fitbit_tokens']
     pushover_tokens = cfg['pushover_tokens']
@@ -27,8 +41,8 @@ def notify(cfg):
 
     po.send_message(formatMsg(getMissingSteps(authd_client)), title="FitBit")
 
-#schedule.every().day.at("20:00").do(notify)
-schedule.every(1).hours.do(notify, cfg=cfg)
+schedule.every().day.at("20:00").do(notify)
+#schedule.every(1).hours.do(notify, cfg=cfg)
 
 notify(cfg)
 while True:
